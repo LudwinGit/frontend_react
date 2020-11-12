65925cecf07462332435b589d2995efe68e8a64e
LudwinGit <ludwinburrion@gmail.com>
2020-11-12 16:04:37 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
+import { Top } from './Graficas/Top';
+import { LastFile } from './Graficas/LastFile';
+import { Pie } from './Graficas/Pie';
+import { Bar } from './Graficas/Bar';
 
 function App() {
+  const url = "http://127.0.0.1:3000/api"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="mb-3 mt-3">
+        <Row>
+          <Col md={12} className="text-center"><h1><b>Coronavirus</b></h1></Col>
+          <Col md={3} >
+            <Top url={url} />
+          </Col>
+          <Col md={9}>
+            <Pie type="pie" name={"barra1"} url={url} />
+          </Col>
+          <Col md={3}>
+            <LastFile url={url} />
+          </Col>
+          <Col md={9}>
+            <Bar name={"barra2"} url={url} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
